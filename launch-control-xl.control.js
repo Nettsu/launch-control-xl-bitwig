7b6a5b68f7a518c8c7e21658599ec98dea2d8380
Netsu <netsu@wro-mxpawl-ml01.local>
2017-09-12 14:44:20 +0200

    Fixed tabs

--- launch-control-xl.control.js
+++ launch-control-xl.control.js
@@ -12,351 +12,348 @@ var buttonMode = ButtonMode.SOLO;
 
 var playbackStates =
 [
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED,
-	PlaybackState.STOPPED
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED,
+    PlaybackState.STOPPED
 ];
 
 var muteStates =
 [
-	false,
-	false,
-	false,
-	false,
-	false,
-	false,
-	false,
-	false
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
 ];
 
 var soloStates =
 [
-	false,
-	false,
-	false,
-	false,
-	false,
-	false,
-	false,
-	false
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
 ];
 
 var recordStates =
 [
-	false,
-	false,
-	false,
-	false,
-	false,
-	false,
-	false,
-	false
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
 ];
 
-var deviceCursors			= [];
-var controlPageCursors 		= [];
-var childTracks 			= [];
-var childTrackCount 		= [];
-var childDeviceCursors		= [];
-var childControlPageCursors	= [];
-var sendBanks				= [];
+var deviceCursors           = [];
+var controlPageCursors      = [];
+var childTracks             = [];
+var childTrackCount         = [];
+var childDeviceCursors      = [];
+var childControlPageCursors = [];
+var sendBanks               = [];
 
 function init()
 {
-	
-	// Setup MIDI in stuff
-	host.getMidiInPort(0).setMidiCallback(onMidi);
+    // Setup MIDI in stuff
+    host.getMidiInPort(0).setMidiCallback(onMidi);
 
-	// create a trackbank (arguments are tracks, sends, scenes)
-	trackBank = host.createMasterTrack(0).createSiblingsTrackBank(NUM_TRACKS, NUM_SENDS, NUM_SCENES, false, false);
-	transport = host.createTransport();
-	transport.getCrossfade().addValueObserver(crossfadeObserver);
+    // create a trackbank (arguments are tracks, sends, scenes)
+    trackBank = host.createMasterTrack(0).createSiblingsTrackBank(NUM_TRACKS, NUM_SENDS, NUM_SCENES, false, false);
+    transport = host.createTransport();
+    transport.getCrossfade().addValueObserver(crossfadeObserver);
 
-	var slotBanks = [];
+    var slotBanks = [];
 
-	for (var i = 0; i < NUM_TRACKS; i++)
-	{
-		// create main device cursor for the track
-		deviceCursors[i] = trackBank.getChannel(i).createDeviceBank(1);
-		controlPageCursors[i] = deviceCursors[i].getDevice(0).createCursorRemoteControlsPage(3);
+    for (var i = 0; i < NUM_TRACKS; i++)
+    {
+        // create main device cursor for the track
+        deviceCursors[i] = trackBank.getChannel(i).createDeviceBank(1);
+        controlPageCursors[i] = deviceCursors[i].getDevice(0).createCursorRemoteControlsPage(3);
 
-		childTracks[i] = trackBank.getChannel(i).createTrackBank(MAX_CHILD_TRACKS, 0, 0, false);
-		childTracks[i].addChannelCountObserver(childrenCountObserver(i));
+        childTracks[i] = trackBank.getChannel(i).createTrackBank(MAX_CHILD_TRACKS, 0, 0, false);
+        childTracks[i].addChannelCountObserver(childrenCountObserver(i));
 
-		// create child track cursors for the track, one for each potential child
-		var childDeviceCursorsArray = [];
-		var childControlPageCursorsArray = [];
-		for (var j = 0; j < MAX_CHILD_TRACKS; j++)
-		{
-			childDeviceCursorsArray[j] = childTracks[i].getChannel(j).createDeviceBank(1);
-			childControlPageCursorsArray[j] = childDeviceCursorsArray[j].getDevice(0).createCursorRemoteControlsPage(3);
-		}
-		childDeviceCursors[i] = childDeviceCursorsArray;
-		childControlPageCursors[i] = childControlPageCursorsArray;
+        // create child track cursors for the track, one for each potential child
+        var childDeviceCursorsArray = [];
+        var childControlPageCursorsArray = [];
+        for (var j = 0; j < MAX_CHILD_TRACKS; j++)
+        {
+            childDeviceCursorsArray[j] = childTracks[i].getChannel(j).createDeviceBank(1);
+            childControlPageCursorsArray[j] = childDeviceCursorsArray[j].getDevice(0).createCursorRemoteControlsPage(3);
+        }
+        childDeviceCursors[i] = childDeviceCursorsArray;
+        childControlPageCursors[i] = childControlPageCursorsArray;
 
-		slotBanks[i] = trackBank.getChannel(i).getClipLauncherSlots();
-		slotBanks[i].addPlaybackStateObserver(playbackObserver(i));
+        slotBanks[i] = trackBank.getChannel(i).getClipLauncherSlots();
+        slotBanks[i].addPlaybackStateObserver(playbackObserver(i));
 
-		trackBank.getChannel(i).getMute().addValueObserver(muteObserver(i));
-		trackBank.getChannel(i).getSolo().addValueObserver(soloObserver(i));
-		trackBank.getChannel(i).getArm().addValueObserver(recordObserver(i));
-		
-		sendBanks[i] = trackBank.getChannel(i).sendBank();
-	}
-	
-	updatePads();
+        trackBank.getChannel(i).getMute().addValueObserver(muteObserver(i));
+        trackBank.getChannel(i).getSolo().addValueObserver(soloObserver(i));
+        trackBank.getChannel(i).getArm().addValueObserver(recordObserver(i));
+        
+        sendBanks[i] = trackBank.getChannel(i).sendBank();
+    }
+    
+    updatePads();
 }
 
 function updatePads()
 {
     for (var i = 0; i < 16; i++)
     {
-		updatePad(i);
-	}
+        updatePad(i);
+    }
 
-	sendMidi(UserPageNotes.Page1, SideButton.SOLO, SideButtonColour[buttonMode == ButtonMode.SOLO ? 1 : 0]);
-	sendMidi(UserPageNotes.Page1, SideButton.MUTE, SideButtonColour[buttonMode == ButtonMode.MUTE ? 1 : 0]);
-	sendMidi(UserPageNotes.Page1, SideButton.RECORD, SideButtonColour[buttonMode == ButtonMode.RECORD ? 1 : 0]);
+    sendMidi(UserPageNotes.Page1, SideButton.SOLO, SideButtonColour[buttonMode == ButtonMode.SOLO ? 1 : 0]);
+    sendMidi(UserPageNotes.Page1, SideButton.MUTE, SideButtonColour[buttonMode == ButtonMode.MUTE ? 1 : 0]);
+    sendMidi(UserPageNotes.Page1, SideButton.RECORD, SideButtonColour[buttonMode == ButtonMode.RECORD ? 1 : 0]);
 }
 
 function updatePad(pad)
 {
-	if (pad < 8)
-	{
-		if (buttonMode == ButtonMode.STOP)
-		{
-			var state = playbackStates[pad];
-			sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], PlaybackStateColour[state]);
-		}
-	}
-	else
-	{
-		if (buttonMode == ButtonMode.MUTE)
-		{
-			println(ButtonReverseMap[pad] + " " + MuteColour[muteStates[pad - 8] ? 1 : 0]);
-			sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], MuteColour[muteStates[pad - 8] ? 1 : 0]);
-		}
-		else if (buttonMode == ButtonMode.SOLO)
-		{
-			sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], SoloColour[soloStates[pad - 8] ? 1 : 0]);
-		}
-		else if (buttonMode == ButtonMode.RECORD)
-		{
-			sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], RecordColour[recordStates[pad - 8] ? 1 : 0]);
-		}
-	}
+    if (pad < 8)
+    {
+        if (buttonMode == ButtonMode.STOP)
+        {
+            var state = playbackStates[pad];
+            sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], PlaybackStateColour[state]);
+        }
+    }
+    else
+    {
+        if (buttonMode == ButtonMode.MUTE)
+        {
+            sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], MuteColour[muteStates[pad - 8] ? 1 : 0]);
+        }
+        else if (buttonMode == ButtonMode.SOLO)
+        {
+            sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], SoloColour[soloStates[pad - 8] ? 1 : 0]);
+        }
+        else if (buttonMode == ButtonMode.RECORD)
+        {
+            sendMidi(UserPageNotes.Page1, ButtonReverseMap[pad], RecordColour[recordStates[pad - 8] ? 1 : 0]);
+        }
+    }
 }
 
 var childrenCountObserver = function(channel)
 {
     var ch = channel;
     return function (count)
-		{
-			if (count > MAX_CHILD_TRACKS)
-			{
-				count = MAX_CHILD_TRACKS;
-			}
-			childTrackCount[ch] = count;
-		}
+        {
+            if (count > MAX_CHILD_TRACKS)
+            {
+                count = MAX_CHILD_TRACKS;
+            }
+            childTrackCount[ch] = count;
+        }
 };
 
 var playbackObserver = function(channel)
 {
     var ch = channel;
     return function (slot, state, queued)
-		{
-			//println(ch + " " + slot + " " + state + " " + queued);
-			if (state == 0 && !queued)
-			{
-				playbackStates[ch] = PlaybackState.STOPPED;
-			}
-			else if (state == 0 && queued)
-			{
-				playbackStates[ch] = PlaybackState.STOPDUE;
-			}
-			else if (state == 1 && queued)
-			{
-				playbackStates[ch] = PlaybackState.QUEUED;
-			}
-			else if (state == 1 && !queued)
-			{
-				playbackStates[ch] = PlaybackState.PLAYING;
-			}
+        {
+            //println(ch + " " + slot + " " + state + " " + queued);
+            if (state == 0 && !queued)
+            {
+                playbackStates[ch] = PlaybackState.STOPPED;
+            }
+            else if (state == 0 && queued)
+            {
+                playbackStates[ch] = PlaybackState.STOPDUE;
+            }
+            else if (state == 1 && queued)
+            {
+                playbackStates[ch] = PlaybackState.QUEUED;
+            }
+            else if (state == 1 && !queued)
+            {
+                playbackStates[ch] = PlaybackState.PLAYING;
+            }
 
-			updatePad(ch);
-		}
+            updatePad(ch);
+        }
 };
 
 var muteObserver = function(channel)
 {
     var ch = channel;
     return function (mute)
-		{
-			muteStates[ch] = mute;
-			updatePad(ch+8);
-		}
+        {
+            muteStates[ch] = mute;
+            updatePad(ch+8);
+        }
 };
 
 var crossfadeObserver = function(value)
 {
-	if (value == 0)
-	{
-		sendMidi(UserPageCCs.Page1, ArrowButton.LEFT, SideButtonColour[1]);
-		sendMidi(UserPageCCs.Page1, ArrowButton.RIGHT, SideButtonColour[0]);
-	}
-	else if (value == 1)
-	{
-		sendMidi(UserPageCCs.Page1, ArrowButton.LEFT, SideButtonColour[0]);
-		sendMidi(UserPageCCs.Page1, ArrowButton.RIGHT, SideButtonColour[1]);
-	}
-	else
-	{
-		sendMidi(UserPageCCs.Page1, ArrowButton.LEFT, SideButtonColour[1]);
-		sendMidi(UserPageCCs.Page1, ArrowButton.RIGHT, SideButtonColour[1]);
-	}
+    if (value == 0)
+    {
+        sendMidi(UserPageCCs.Page1, ArrowButton.LEFT, SideButtonColour[1]);
+        sendMidi(UserPageCCs.Page1, ArrowButton.RIGHT, SideButtonColour[0]);
+    }
+    else if (value == 1)
+    {
+        sendMidi(UserPageCCs.Page1, ArrowButton.LEFT, SideButtonColour[0]);
+        sendMidi(UserPageCCs.Page1, ArrowButton.RIGHT, SideButtonColour[1]);
+    }
+    else
+    {
+        sendMidi(UserPageCCs.Page1, ArrowButton.LEFT, SideButtonColour[1]);
+        sendMidi(UserPageCCs.Page1, ArrowButton.RIGHT, SideButtonColour[1]);
+    }
 }
 
 var soloObserver = function(channel)
 {
     var ch = channel;
     return function (solo)
-		{
-			soloStates[ch] = solo;
-			updatePad(ch+8);
-		}
+        {
+            soloStates[ch] = solo;
+            updatePad(ch+8);
+        }
 };
 
 var recordObserver = function(channel)
 {
     var ch = channel;
     return function (record)
-		{
-			recordStates[ch] = record;
-			updatePad(ch+8);
-		}
+        {
+            recordStates[ch] = record;
+            updatePad(ch+8);
+        }
 };
 
 function processSideButtons(status, data1, data2)
 {
-	if (status == UserPageNotes.Page1)
-	{
-		if (data1 == SideButton.MUTE && data2 == 127)
-		{
-			buttonMode = ButtonMode.MUTE;
-			updatePads();
-		}
-		else if (data1 == SideButton.SOLO && data2 == 127)
-		{
-			buttonMode = ButtonMode.SOLO;
-			updatePads();
-		}
-		else if (data1 == SideButton.RECORD && data2 == 127)
-		{
-			buttonMode = ButtonMode.RECORD;
-			updatePads();
-		}
-	}
-	else if (status == UserPageCCs.Page1)
-	{	
-		if (data1 == ArrowButton.UP && data2 == 127)
-		{
-			transport.getTempo().incRaw(1);
-		}
-		else if (data1 == ArrowButton.DOWN && data2 == 127)
-		{
-			transport.getTempo().incRaw(-1);
-		}
-		else if (data1 == ArrowButton.LEFT && data2 == 127)
-		{
-			transport.getCrossfade().set(0, 128);
-		}
-		else if (data1 == ArrowButton.RIGHT && data2 == 127)
-		{
-			transport.getCrossfade().set(127, 128);
-		}
-	}
+    if (status == UserPageNotes.Page1)
+    {
+        if (data1 == SideButton.MUTE && data2 == 127)
+        {
+            buttonMode = ButtonMode.MUTE;
+            updatePads();
+        }
+        else if (data1 == SideButton.SOLO && data2 == 127)
+        {
+            buttonMode = ButtonMode.SOLO;
+            updatePads();
+        }
+        else if (data1 == SideButton.RECORD && data2 == 127)
+        {
+            buttonMode = ButtonMode.RECORD;
+            updatePads();
+        }
+    }
+    else if (status == UserPageCCs.Page1)
+    {   
+        if (data1 == ArrowButton.UP && data2 == 127)
+        {
+            transport.getTempo().incRaw(1);
+        }
+        else if (data1 == ArrowButton.DOWN && data2 == 127)
+        {
+            transport.getTempo().incRaw(-1);
+        }
+        else if (data1 == ArrowButton.LEFT && data2 == 127)
+        {
+            transport.getCrossfade().set(0, 128);
+        }
+        else if (data1 == ArrowButton.RIGHT && data2 == 127)
+        {
+            transport.getCrossfade().set(127, 128);
+        }
+    }
 }
 
 function onMidi(status, data1, data2)
 {
-	//printMidi(status, data1, data2);
-	//println(MIDIChannel(status));
+    //printMidi(status, data1, data2);
 
-	processSideButtons(status, data1, data2);
+    processSideButtons(status, data1, data2);
 
-	// Buttons
-	if (status == UserPageNotes.Page1 && ButtonMap[data1] >= 0 && ButtonMap[data1] <= 15)
+    // Buttons
+    if (status == UserPageNotes.Page1 && ButtonMap[data1] >= 0 && ButtonMap[data1] <= 15)
     {
-		var button = ButtonMap[data1];
-		var ch = button % 8;
-		
-		if (button < 8)
-		{
-			trackBank.getChannel(ch).stop();
-			// hack for groups that don't send stop info to observer
-			if (playbackStates[ch] == PlaybackState.PLAYING)
-			{
-				playbackStates[ch] = PlaybackState.STOPDUE;
-			}
-		}
-		else
-		{
-			if (buttonMode == ButtonMode.MUTE)
-			{
-				trackBank.getChannel(ch).getMute().toggle();
-			}
-			else if (buttonMode == ButtonMode.SOLO)
-			{
-				trackBank.getChannel(ch).getSolo().toggle(false);
-			}
-			else if (buttonMode == ButtonMode.RECORD)
-			{
-				trackBank.getChannel(ch).getArm().toggle();
-			}
-		}
-		updatePad(button);
-	}
-	// First two knobs control first two macros of each channel
-	else if (status == UserPageCCs.Page1 && KnobMap[data1] >= 0 && KnobMap[data1] < 16)
-	{
-		var channelIdx = KnobMap[data1] % 8;
-		var macro = KnobMap[data1] / 8;
+        var button = ButtonMap[data1];
+        var ch = button % 8;
+        
+        if (button < 8)
+        {
+            trackBank.getChannel(ch).stop();
+            // hack for groups that don't send stop info to observer
+            if (playbackStates[ch] == PlaybackState.PLAYING)
+            {
+                playbackStates[ch] = PlaybackState.STOPDUE;
+            }
+        }
+        else
+        {
+            if (buttonMode == ButtonMode.MUTE)
+            {
+                trackBank.getChannel(ch).getMute().toggle();
+            }
+            else if (buttonMode == ButtonMode.SOLO)
+            {
+                trackBank.getChannel(ch).getSolo().toggle(false);
+            }
+            else if (buttonMode == ButtonMode.RECORD)
+            {
+                trackBank.getChannel(ch).getArm().toggle();
+            }
+        }
+        updatePad(button);
+    }
+    // First two knobs control first two macros of each channel
+    else if (status == UserPageCCs.Page1 && KnobMap[data1] >= 0 && KnobMap[data1] < 16)
+    {
+        var channelIdx = KnobMap[data1] % 8;
+        var macro = KnobMap[data1] / 8;
 
-		//println(childTrackCount[channelIdx]);
+        //println(childTrackCount[channelIdx]);
 
-		deviceCursors[channelIdx].scrollTo(0);
-		controlPageCursors[channelIdx].getParameter(macro).set(data2, 128);
-		for (var i = 0; i < childTrackCount[channelIdx]; i++)
-		{
-			childDeviceCursors[channelIdx][i].scrollTo(0);
-			childControlPageCursors[channelIdx][i].getParameter(macro).set(data2, 128);
-		}
-	}
-	// the third knob controls the first send
-	else if (status == UserPageCCs.Page1 && KnobMap[data1] >= 16 && KnobMap[data1] < 24)
-	{
-		var channelIdx = KnobMap[data1] % 8;
-		var send = 0;
+        deviceCursors[channelIdx].scrollTo(0);
+        controlPageCursors[channelIdx].getParameter(macro).set(data2, 128);
+        for (var i = 0; i < childTrackCount[channelIdx]; i++)
+        {
+            childDeviceCursors[channelIdx][i].scrollTo(0);
+            childControlPageCursors[channelIdx][i].getParameter(macro).set(data2, 128);
+        }
+    }
+    // the third knob controls the first send
+    else if (status == UserPageCCs.Page1 && KnobMap[data1] >= 16 && KnobMap[data1] < 24)
+    {
+        var channelIdx = KnobMap[data1] % 8;
+        var send = 0;
 
-		sendBanks[i].getItemAt(send).set(data2, 128);
-	}
-	// Faders
-	else if (status == UserPageCCs.Page1 && FaderMap[data1] >= 0 && FaderMap[data1] < 8)
-	{
-		var channelIdx = FaderMap[data1];
-		
-		// to map the faders to 0db maximum, set the max value in the XL Editor to 102
-		trackBank.getChannel(channelIdx).getVolume().set(data2, 128);
-		for (var i = 0; i < childTrackCount[channelIdx]; i++)
-		{		
-			childTracks[channelIdx].getChannel(i).getVolume().set(data2, 128);
-		}
-	}
+        sendBanks[i].getItemAt(send).set(data2, 128);
+    }
+    // Faders
+    else if (status == UserPageCCs.Page1 && FaderMap[data1] >= 0 && FaderMap[data1] < 8)
+    {
+        var channelIdx = FaderMap[data1];
+        
+        // to map the faders to 0db maximum, set the max value in the XL Editor to 102
+        trackBank.getChannel(channelIdx).getVolume().set(data2, 128);
+        for (var i = 0; i < childTrackCount[channelIdx]; i++)
+        {       
+            childTracks[channelIdx].getChannel(i).getVolume().set(data2, 128);
+        }
+    }
 }
 
 function exit()
